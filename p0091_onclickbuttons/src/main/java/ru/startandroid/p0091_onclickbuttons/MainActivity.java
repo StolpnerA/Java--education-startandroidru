789d46aa80998e36e85857bc376f1b15956e70bc
Andrey Stolpner <andreysx@tut.by>
2018-10-02 09:34:51 +0300

    finish lesson 9 (add listener click on btn)

--- p0091_onclickbuttons/src/main/java/ru/startandroid/p0091_onclickbuttons/MainActivity.java
+++ p0091_onclickbuttons/src/main/java/ru/startandroid/p0091_onclickbuttons/MainActivity.java
@@ -2,6 +2,7 @@ package ru.startandroid.p0091_onclickbuttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
         tvOut = (TextView) findViewById(R.id.tvOut);
         btnOk = (Button) findViewById(R.id.btnOk);
         btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+
+        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Нажата кнопка Cancel");
+            }
+        };
+
+
+        btnOk.setOnClickListener(oclBtnOk);
+        btnCancel.setOnClickListener(oclBtnCancel);
     }
 }
